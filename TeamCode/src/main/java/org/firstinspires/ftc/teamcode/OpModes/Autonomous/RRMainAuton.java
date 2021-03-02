f566aa2db127d8ff4620309461500d6ac9d37505
gumbymoo <julian.b.shah@gmail.com>
2021-03-02 16:19:49 -0600

    added new main auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/RRMainAuton.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class RRMainAuton extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+    }
+
+}
