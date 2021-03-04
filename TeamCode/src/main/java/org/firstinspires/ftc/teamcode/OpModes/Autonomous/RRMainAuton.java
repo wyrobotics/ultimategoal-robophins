ef372154e2e778d45b79f23693408c6bfc59d243
Julian <julian.b.shah@gmail.com>
2021-03-03 20:38:48 -0600

    new auton now has THINGS in it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/RRMainAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/RRMainAuton.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Components.RRMainRobot;
@@ -23,6 +26,7 @@ public class RRMainAuton extends LinearOpMode {
 
         mainRobot = new RRMainRobot(hardwareMap, telemetry);
 
+
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
 
@@ -34,6 +38,7 @@ public class RRMainAuton extends LinearOpMode {
 
         //phoneCam.setFlashlightEnabled(true);
 
+
         mainRobot.wobbleGoalArm.grab();
 
         mainRobot.shooter.setpoint = 0;
@@ -41,6 +46,10 @@ public class RRMainAuton extends LinearOpMode {
         mainRobot.shooter.initPos();
 
         mainRobot.shooter.enableController();
+
+
+        Pose2d initPose = new Pose2d(0, 0, Math.toRadians(180));
+
 
         waitForStart();
 
@@ -52,6 +61,46 @@ public class RRMainAuton extends LinearOpMode {
         telemetry.update();
 
         phoneCam.stopStreaming();
+
+        switch((int) height) {
+            case 0:
+
+                mainRobot.drive.setPoseEstimate(initPose);
+
+                Trajectory dropWobble = mainRobot.drive.trajectoryBuilder(initPose)
+                        .splineToConstantHeading(new Vector2d(6, -24), Math.toRadians(180))
+                        .splineToConstantHeading(new Vector2d(60, -24), Math.toRadians(180))
+                        .build();
+
+                mainRobot.drive.followTrajectory(dropWobble);
+
+                mainRobot.deng(1000);
+
+                mainRobot.wobbleGoalArm.down();
+                mainRobot.deng(1000);
+                mainRobot.wobbleGoalArm.release();
+                mainRobot.deng(500);
+                mainRobot.wobbleGoalArm.lift();
+                mainRobot.deng(500);
+
+                Trajectory toPowerShot = mainRobot.drive.trajectoryBuilder(dropWobble.end())
+                        .splineTo(new Vector2d(60, 24), 0)
+                        .build();
+
+                Trajectory powerShotStrafe1 = mainRobot.drive.trajectoryBuilder(toPowerShot.end())
+                        .strafeLeft(4)
+                        .build();
+
+
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+        }
 
 
 
